243549e0733e5d56f65d4aa471f5b70cb36eaa73
Александр Куликов <159744787+KulikovAR@users.noreply.github.com>
2024-03-25 11:30:04 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
+    
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if(config('app.env') === 'production' || config('app.env') === 'dev') {
-           \Url::forceScheme('https');
+           Url::forceScheme('https');
         }
     }
 }
