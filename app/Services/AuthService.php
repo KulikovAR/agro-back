5ab070f7fa96e9e06c9e2a2ae10c1c1d9d16f6ab
Ruslan Guskov <feniksuh@gmail.com>
2024-05-01 17:07:07 +0300

    task-userprofile roles auth fix

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -43,7 +43,7 @@ class AuthService
         $code_arr = $this->sms->setCode();
         $clientRole = Role::where('name', 'client')->first();
         $user = User::firstOrCreate(['phone_number' => $request->phone_number],['phone_number' => $request->phone_number]);
-        $user->assignRole($clientRole);
+        $user->syncRoles([$clientRole]);
         if (env('APP_ENV') == 'production') {
             $this->sms->send($request->phone_number, $code_arr['code'] . '- Verification code Cargis');
             $resource = new UserResource($user);
