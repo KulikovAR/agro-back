e154cfa50ee6053cdbe8926abd6d1013cdea5341
Flifo <76399317+RGO230@users.noreply.github.com>
2024-08-09 12:40:52 +0300

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -36,7 +36,17 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         if (App::environment(EnvironmentTypeEnum::productEnv())) {
-            return;
+             $admin = User::create([
+            'email'             => 'oooagrologistika@admin.ru',
+            'phone_number'      => '79202149563',
+            'password'          => 'dima12345',
+            'phone_verified_at' => Carbon::now()
+        ]);
+
+        $admin_role = Role::where('name', 'admin')->first();
+        $admin->assignRole($admin_role);
+        $Ic = User::create([]);
+        $Ic->assignRole(RoleEnum::IC->value);
         }
         $clientRole = Role::where('slug', 'client')->first();
 
