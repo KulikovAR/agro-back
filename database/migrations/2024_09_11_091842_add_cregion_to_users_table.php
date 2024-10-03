c828b1a200319ec2598185ce2fc641c83cbbed61
KulikovAR <106818844+SashkaBackEnd@users.noreply.github.com>
2024-10-03 16:58:51 +0300

    hotfix: drop unique in region

--- database/migrations/2024_09_11_091842_add_cregion_to_users_table.php
+++ database/migrations/2024_09_11_091842_add_cregion_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('cregion')->unique()->nullable();
+            $table->string('cregion')->nullable();
 
         });
     }
