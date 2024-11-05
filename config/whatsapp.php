60a7e05d1e5819a5ddf5899471317a487fbce6af
KulikovAR <106818844+SashkaBackEnd@users.noreply.github.com>
2024-11-05 16:21:10 +0300

    config fixies

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -7,7 +7,7 @@ return [
 
     'api_token_2'  => env('WHATS_APP_API_TOKEN_2', null),
     'profile_id_2' => env('WHATS_APP_PROFILE_ID_2', null),
-    'phone_2'      => env('WHATS_APP_PHONE', null),
+    'phone_2'      => env('WHATS_APP_PHONE_2', null),
 
     'host'         => env('WHATS_APP_HOST', null),
 ];
